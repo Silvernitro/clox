bbeedee2b083101a6972c4c53860e8e908f81abd
Lau Siaw Sam <lausiawsam98@gmail.com>
2020-12-30 20:58:01 +0800

    Implement setting of key-val in hash table

--- table.c
+++ table.c
@@ -1,6 +1,8 @@
 #include "table.h"
 
 #include "memory.h"
+#include "object.h"
+#include "value.h"
 
 void initTable(Table* table) {
   table->count = 0;
@@ -11,4 +13,65 @@ void initTable(Table* table) {
 void freeTable(Table* table) {
   FREE_ARRAY(Entry, table->entries, table->capacity);
   initTable(table);
+}
+
+static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
+  uint32_t index = key->hash % capacity;
+
+  for (;;) {
+    Entry* entry = &entries[index];
+
+    if (entry->key == NULL || entry->key == key) {
+      return entry;
+    }
+
+    index = (index + 1) % capacity;
+  }
+}
+
+static void adjustCapacity(Table* table, int capacity) {
+  Entry* entries = ALLOCATE(Entry, capacity);
+  for (int i = 0; i < capacity; i++) {
+    entries[i].key = NULL;
+    entries[i].value = NIL_VAL();
+  }
+
+  for (int i = 0; i < table->capacity; i++) {
+    Entry* entry = &table->entries[i];
+    if (entry->key == NULL) continue;
+
+    Entry* newEntry = findEntry(entries, capacity, entry->key);
+    newEntry->key = entry->key;
+    newEntry->value = entry->value;
+  }
+
+  FREE_ARRAY(Entry, table->entries, table->capacity);
+  table->capacity = capacity;
+  table->entries = entries;
+}
+
+bool tableSet(Table* table, ObjString* key, Value value) {
+  if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
+    int newCapacity = GROW_CAPACITY(table->capacity);
+  }
+
+  Entry* entry = findEntry(table->entries, table->capacity, key);
+
+  bool isNewKey = entry->key == NULL;
+  if (isNewKey) {
+    table->count++;
+  }
+
+  entry->key = key;
+  entry->value = value;
+  return isNewKey;
+}
+
+void tableAddAll(Table* fromTable, Table* toTable) {
+  for (int i = 0; i < fromTable->capacity; i++) {
+    Entry* entry = &fromTable->entries[i];
+    if (entry->key != NULL) {
+      tableSet(toTable, entry->key, entry->value);
+    }
+  }
 }
--- table.h
+++ table.h
@@ -4,6 +4,8 @@
 #include "common.h"
 #include "value.h"
 
+#define TABLE_MAX_LOAD 0.75
+
 typedef struct {
   ObjString* key;
   Value value;
@@ -17,5 +19,6 @@ typedef struct {
 
 void initTable(Table* table);
 void freeTable(Table* table);
+bool tableSet(Table* table, ObjString* key, Value value);
 
 #endif
